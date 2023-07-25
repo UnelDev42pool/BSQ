6759d649b8dcb2d72e75fa2deb53f06972e889e9
Emile Dechenaud <edechena@z4r12p5.42lyon.fr>
2023-07-25 15:55:47 +0200

    Cleaning header

--- includes/bsq.h
+++ includes/bsq.h
@@ -6,7 +6,7 @@
 /*   By: edechena <edechena@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 10:11:10 by edechena          #+#    #+#             */
-/*   Updated: 2023/07/25 11:25:36 by edechena         ###   ########lyon.fr   */
+/*   Updated: 2023/07/25 15:48:38 by edechena         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <fcntl.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE	100000000
+#  define BUFFER_SIZE	1000000
 # endif
 
 # define BUFFER_INIT	4096
@@ -27,34 +27,37 @@
 
 typedef struct s_data
 {
-	char	*map;
-	char	empty;
-	char	obstacle;
-	char	filler;
-	int		nbr_lines;
-	int		len_lines;
 	int		bsq_x;
 	int		bsq_y;
-	long	n;
+	char	empty;
 	int		fd;
+	char	filler;
+	int		len_lines;
+	char	*map;
+	long	n;
+	int		nbr_lines;
+	char	obstacle;
 }	t_data;
 
-bool			read_file(t_data *d, char *file);
-bool			read_stdin(t_data *d);
 bool			check_map(t_data *d);
 bool			read_len_lines(t_data *d);
 
-bool			process(t_data *d, unsigned short **matrix);
-
-void			display_bsq(t_data *d, unsigned short **matrix);
-
-void			init_data(t_data *d);
-unsigned short	**init_matrix(t_data *d);
 void			free_matrix(unsigned short **matrix, t_data *d);
 unsigned short	**free_matrix_i(unsigned short **matrix, int i);
 
+void			ft_putstr(char *str);
+
 char			*ft_strjoin_bsq(char *s1, const int size1,
 					char *s2, const int size2);
-void			ft_putstr(char *str);
+
+void			init_data(t_data *d);
+unsigned short	**init_matrix(t_data *d);
+
+void			display_bsq(t_data *d, unsigned short **matrix);
+
+bool			process(t_data *d, unsigned short **matrix);
+
+bool			read_file(t_data *d, char *file);
+bool			read_stdin(t_data *d);
 
 #endif
